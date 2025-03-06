7dd456ffa62f95d90704622e5e1056aa499d5e38
icerayer <icerayer.og@gmail.com>
2025-03-06 13:58:27 -0500

    added league page

---
+++ src/_components/LeagueNav.tsx
@@ -0,0 +1,9 @@
+export function LeagueNav() {
+    return (
+        <nav className="flex justify-center w-full p-4 gap-4 font-semibold text-xl bg-white shadow-md">
+            <div>League</div>
+            <div>Team</div>
+            <div>Players</div>
+        </nav>
+    );
+}
---
+++ src/app/league/page.tsx
@@ -0,0 +1,9 @@
+export default function LeaguePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#12026d] to-[#15162c] text-white">
+        <div>
+            <h1>League Page</h1>
+        </div>
+    </main>
+  );
+}
