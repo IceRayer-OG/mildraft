20584865f94fedffaf61c904c0d0a546f201121c
icerayer <icerayer.og@gmail.com>
2025-03-06 12:37:40 -0500

    icon and schema update

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,6 +8,7 @@ import {
   pgTableCreator,
   timestamp,
   varchar,
+  boolean,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -32,5 +33,137 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
+  })
+);
+
+export const leagues = createTable(
+  "league",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }),
+    commissioner: varchar("commissioner", { length: 256 }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("league_idx").on(example.name),
+  })
+);
+
+export const teams = createTable(
+  "team",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }),
+    leagueId: integer("league_id").notNull(),
+    owernId: integer("owner_id"),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("team_idx").on(example.name),
+  })
+);
+
+export const players = createTable(
+  "player",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }),
+    teamId: integer("team_id").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("player_idx").on(example.name),
+  })
+);
+
+  export const drafts = createTable(
+  "draft",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    name: varchar("name", { length: 256 }),
+    leagueId: integer("league_id").notNull(),
+    teamId: integer("team_id").notNull(),
+    playerId: integer("player_id").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("draft_idx").on(example.name),
+  })
+);
+
+export const draftPicks = createTable(
+  "draft_pick",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    draftId: integer("draft_id").notNull(),
+    playerId: integer("player_id").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("draft_pick_idx").on(example.id),
+  })
+);
+
+export const pros = createTable(
+  "pros",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    playerId: integer("player_id").notNull(),
+    playerFirstName: varchar("player_first_name", { length: 256 }),
+    playerLastName: varchar("player_last_name", { length: 256 }),
+    position: varchar("position", { length: 256 }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("pros_idx").on(example.id),
+  })
+);
+
+export const settings = createTable(
+  "settings",
+  {
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    leagueId: integer("league_id").notNull(),
+    draftsEnabled: boolean("drafts_enabled").notNull(),
+    draftType: varchar("draft_type", { length: 256 }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (example) => ({
+    nameIndex: index("settings_idx").on(example.id),
   })
 );
