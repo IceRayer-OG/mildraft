c46c820e3f61f4a5ac08aea6a5e4d19ee2a89aee
icerayer <icerayer.og@gmail.com>
2025-03-17 19:48:59 -0400

    settings page basics

--- src/app/league/settings/page.tsx
+++ src/app/league/settings/page.tsx
@@ -1,1 +1,11 @@
 // going to be the settings page for the league
+
+export default function SettingsPage() {
+  return (
+    <div className="flex flex-col justify-between w-full min-h-screen p-4 bg-gradient-to-b from-[#12026d] to-[#15162c] text-white">
+        <p>League Settings</p>
+        {/* the settings page */}
+        <p>The Core Content. </p>
+    </div>
+  )
+}   
