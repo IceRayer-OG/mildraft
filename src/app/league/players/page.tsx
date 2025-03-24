3ad072f738002f934a8e459808665e3c8e1d2220
icerayer <icerayer.og@gmail.com>
2025-03-24 15:58:46 -0400

    Players update and read me

--- src/app/league/players/page.tsx
+++ src/app/league/players/page.tsx
@@ -1,31 +1,36 @@
-import { type Players, columns } from "./columns"
-import { DataTable } from "~/_components/data-table"
+
+
+import { getFreeAgents } from "~/server/queries";
+import { type Players} from "~/utils/players";
+import { PlayerDataTable } from "~/features/players/components/player-data-table";
+import { playerColumns } from "~/features/players/components/player-columns";
  
 async function getData(): Promise<Players[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52t",
-      playerName: "Luke Skywalker",
-      position: "P",
-      team: "Milwaukee Brewers",
-      age: 25,
-      height: "5' 10''",
-      weight: 170,
-      throws: "R",
-      bats: "R",
-    },
-    // ...
-  ]
+  // Fetch data from your API here
+  const freeAgents = await getFreeAgents() as Players[];
+
+  return freeAgents;
+
+  // Placeholder data for demonstration purposes
+  // return [
+  //   {
+  //     id: 5,
+  //     playerName: "Luke Skywalker",
+  //     position: "P",
+  //     team: "Milwaukee Brewers",
+  //     throws: "R",
+  //     bats: "R",
+  //   },
+  // ]
 }
  
 export default async function DemoPage() {
   const data = await getData()
  
   return (
-    <main className="flex min-h-screen flex-col p-4 bg-gradient-to-b from-[#12026d] to-[#15162c] text-white">
-        <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={data} />
+    <main className="flex flex-col min-h-screen w-full items-center p-4 bg-gradient-to-b from-[#12026d] to-[#15162c] text-white">
+        <div className="grow w-full">
+            <PlayerDataTable columns={playerColumns} data={data} />
         </div>
     </main>
   )
---
+++ src/app/league/team/settings/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function TeamSettingsPage() {
+  return (
+    <div>
+      <h1>Team Settings</h1>
+      <p>Upload your team logo</p>
+      <p>Team Name</p>
+      <p>Team Abbreviation</p>
+      <p>Team Color</p>
+      
+    </div>
+  );
+}
