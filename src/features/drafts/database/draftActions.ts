7c90c670bd114b991f5aa9f7f57226efc5bf6f83
IceRayer <icerayer.og@gmail.com>
2025-07-18 16:57:18 +0000

    deploy error

--- src/features/drafts/database/draftActions.ts
+++ src/features/drafts/database/draftActions.ts
@@ -13,7 +13,7 @@ import {
 import { type DraftPlayers, type QueuePlayers } from "~/utils/players";
 import { type QueueDraftPick } from "~/utils/draft";
 import { revalidatePath } from "next/cache";
-import { auth } from "@clerk/nextjs/server";
+// import { auth } from "@clerk/nextjs/server";
 
 export async function dbQueuePlayer(playerToQueue: DraftPlayers) {
   // DB Call to add player to queue
@@ -32,20 +32,20 @@ export async function dbRemovePlayerFromQueue(playerToRemove: QueuePlayers) {
   revalidatePath("league/draft");
 }
 
-export async function dbDraftPlayer(playerToDraft: DraftPlayers) {
-  // Get Current Pick
-  const currentPick = await getCurrentDraftPick();
-  if(currentPick?.teamId === null || currentPick?.teamId === undefined) throw new Error("No current pick found");
-  if(currentPick?.pickNumber === null || currentPick?.pickNumber === undefined) throw new Error("No current pick found");
-  // Check if the user is the team owner
-  const teamOwner = getTeamOwner(currentPick.teamId);
-  if(teamOwner === null) throw new Error("No team owner found");
-  if(!teamOwner) throw new Error("You are not the team owner");
+// export async function dbDraftPlayer(playerToDraft: DraftPlayers) {
+//   // Get Current Pick
+//   const currentPick = await getCurrentDraftPick();
+//   if(currentPick?.teamId === null || currentPick?.teamId === undefined) throw new Error("No current pick found");
+//   if(currentPick?.pickNumber === null || currentPick?.pickNumber === undefined) throw new Error("No current pick found");
+//   // Check if the user is the team owner
+//   const teamOwner = getTeamOwner(currentPick.teamId);
+//   if(teamOwner === null) throw new Error("No team owner found");
+//   if(!teamOwner) throw new Error("You are not the team owner");
 
-  // DB Call to draft player
-  await postDraftPick(playerToDraft.id, currentPick.pickNumber);
-  revalidatePath("league/draft");
-}
+//   // DB Call to draft player
+//   await postDraftPick(playerToDraft.id, currentPick.pickNumber);
+//   revalidatePath("league/draft");
+// }
 
 export async function dbGetDraftPlayers() {
   // Data fetch draft players
