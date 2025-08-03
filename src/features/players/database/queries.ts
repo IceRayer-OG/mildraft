e752b1246b55881841411bf80bdaed5c156aaa51
IceRayer <icerayer.og@gmail.com>
2025-08-03 11:20:46 +0000

    Free agent query update

--- src/features/players/database/queries.ts
+++ src/features/players/database/queries.ts
@@ -1,9 +1,29 @@
 import { db } from "~/server/db";
-import { eq, notInArray, and, isNotNull, isNull, asc, inArray } from "drizzle-orm";
-import { queues, draftPicks, pros, teams } from "~/server/db/schema";
+import { eq, notInArray, and, isNotNull, isNull, asc, inArray, or } from "drizzle-orm";
+import { queues, draftPicks, pros, teams, players } from "~/server/db/schema";
 
 export async function getFreeAgents() {
+    const playersDrafted = db.select({pickedPlayer: draftPicks.playerId}).from(draftPicks)
+        .where(
+            and(
+                eq(draftPicks.draftId, 2), 
+                isNotNull(draftPicks.playerId),
+            )
+        );
+    const playersOnTeams = db.select({playerOnTeam: players.proId}).from(players)
+        .where(eq(players.leagueId, 1));
+
     const freeAgents = await db.select().from(pros)
+        .where(
+            and(
+                notInArray(pros.id, playersDrafted),
+                notInArray(pros.id, playersOnTeams)
+            )
+        );
+        
+    if (freeAgents === null) throw new Error("Error getting free agents"); 
+
+    console.log("Free agents:", freeAgents);
 
     return freeAgents;
 }
