321e1ecc206490d0351bef31e7e32e313d1ea27f
IceRayer <icerayer.og@gmail.com>
2025-08-12 02:15:29 +0000

    Added claim team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,37 @@
-// import { Button } from "~/_components/ui/button"
-// import Link from "next/link";
+import { ClaimTeamDialog } from "~/features/team/components/ClaimTeamDialog";
+import { getLeagueUnclaimedTeamsAction } from "~/features/team/actions/teamActions";
 
 export default function HomePage() {
+  const unclaimedLeagueTeams = getLeagueUnclaimedTeamsAction();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#12026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          SV Baseball <span className="text-[hsl(280,100%,70%)]">MiL Draft</span> App
+          SV Baseball{" "}
+          <span className="text-[hsl(280,100%,70%)]">MiL Draft</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Basics are scaffolded for league managers
-              Draf functionality working.
-              Mobile friendly!
+              Basics are scaffolded for league managers Draf functionality
+              working. Mobile friendly!
             </div>
           </div>
           <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">Documentation →</h3>
             <div className="text-lg">
-              Setup for documentation with the app, 
-              guiding users on how to use it.
+              Setup for documentation with the app, guiding users on how to use
+              it.
             </div>
           </div>
+        </div>
+        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
+          <ClaimTeamDialog unclaimedLeagueTeams={unclaimedLeagueTeams} />
         </div>
       </div>
     </main>
   );
 }
+
---
+++ src/features/team/components/ClaimTeamDialog.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Suspense } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+} from "~/_components/ui/dialog";
+import { Button } from "~/_components/ui/button";
+import ClaimTeamList from "./ClaimTeamsList";
+import { type Team } from "../utils/team";
+
+export function ClaimTeamDialog({unclaimedLeagueTeams}:{unclaimedLeagueTeams: Promise<Team[]>}) {
+  
+	return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"ghost"}>Claim Your Team</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogTitle className="justify-self-center">Claim Your Team</DialogTitle>
+        <Suspense fallback={<div>Loading...</div>}>
+				  <ClaimTeamList teamList={unclaimedLeagueTeams} />
+        </Suspense>
+      </DialogContent>
+    </Dialog>
+  );
+}
