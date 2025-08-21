c73ee6e90e03a6300f3dc9c73399b4112796bd00
IceRayer <icerayer.og@gmail.com>
2025-08-21 13:56:50 +0000

    Adding some flare with motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ export default function HomePage() {
           SV Baseball{" "}
           <span className="text-[hsl(280,100%,70%)]">MiL Draft</span> App
         </h1>
+        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-zinc-900/80 p-6">
+            <ClaimTeamDialog unclaimedLeagueTeams={unclaimedLeagueTeams} />
+        </div>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">First Steps →</h3>
@@ -29,9 +32,7 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-            <ClaimTeamDialog unclaimedLeagueTeams={unclaimedLeagueTeams} />
-        </div>
+        
       </div>
     </main>
   );
--- src/features/team/components/ClaimTeamDialog.tsx
+++ src/features/team/components/ClaimTeamDialog.tsx
@@ -9,21 +9,50 @@ import {
   DialogTitle,
 } from "~/_components/ui/dialog";
 import { Button } from "~/_components/ui/button";
+import { GlowEffect } from "~/_components/ui/glow-effect";
+import { TextShimmerWave } from "~/_components/ui/text-shimmer-wave";
 import ClaimTeamList from "./ClaimTeamsList";
 import { type UnclaimedTeam } from "../utils/team";
 
-
-export function ClaimTeamDialog({unclaimedLeagueTeams,}:{unclaimedLeagueTeams: Promise<UnclaimedTeam[]>}) {
-  
-	return (
+export function ClaimTeamDialog({
+  unclaimedLeagueTeams,
+}: {
+  unclaimedLeagueTeams: Promise<UnclaimedTeam[]>;
+}) {
+  return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant={"ghost"}>Claim Your Team</Button>
+        <div className="relative">
+          <GlowEffect
+            colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
+            mode="colorShift"
+            blur="soft"
+            duration={3}
+            scale={0.9}
+          />
+          <Button
+            variant={"ghost"}
+            className="relative inline-flex items-center gap-1 rounded-md bg-zinc-900 px-2.5 py-1.5"
+          >
+            <TextShimmerWave
+              className="[--base-color:#FFFFFF] [--base-gradient-color:#5EB1EF]"
+              duration={1}
+              spread={1}
+              zDistance={1}
+              scaleDistance={1.1}
+              rotateYDistance={20}
+            >
+              Claim Your Team
+            </TextShimmerWave>
+          </Button>
+        </div>
       </DialogTrigger>
       <DialogContent>
-        <DialogTitle className="justify-self-center">Claim Your Team</DialogTitle>
+        <DialogTitle className="justify-self-center">
+          Claim Your Team
+        </DialogTitle>
         <Suspense fallback={<div>Loading...</div>}>
-				  <ClaimTeamList teamList={unclaimedLeagueTeams} />
+          <ClaimTeamList teamList={unclaimedLeagueTeams} />
         </Suspense>
       </DialogContent>
     </Dialog>
