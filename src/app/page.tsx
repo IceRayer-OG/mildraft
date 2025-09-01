59b9aabc2a6b7fef34bbe3924bed35644cc500eb
IceRayer <icerayer.og@gmail.com>
2025-09-01 02:08:30 +0000

    Homepage edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
           <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Basics are scaffolded for league managers Draf functionality
+              Basics are scaffolded for league managers Draft functionality
               working. Mobile friendly!
             </div>
           </div>
